e5a9aaeb056b64304af846c81f710652fc8859f6
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-11-17 08:45:29 +0000

    add db context to Api

--- Capstone.Api/Program.cs
+++ Capstone.Api/Program.cs
@@ -1,5 +1,10 @@
+using Capstone.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<TaskDb>(opt => 
+    opt.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TaskDB;Trusted_Connection=True;"));
 
 var app = builder.Build();
 
