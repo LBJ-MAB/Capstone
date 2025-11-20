69ce4f159a0a7c7aa85e7fa65ee3270f129c271a
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-11-20 08:17:09 +0000

    unit test

--- Capstone.Api/obj/Debug/net9.0/Capstone.Api.AssemblyInfo.cs
+++ Capstone.Api/obj/Debug/net9.0/Capstone.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ffd5bf727794fc3369db7776457082e9affe99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d65818ce0f771175e3bafa721d02ecd205b509")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Capstone.Domain/obj/Debug/net9.0/Capstone.Domain.AssemblyInfo.cs
+++ Capstone.Domain/obj/Debug/net9.0/Capstone.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e23c8ec803fb6456417b2bfa021a5245db8d49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d65818ce0f771175e3bafa721d02ecd205b509")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
